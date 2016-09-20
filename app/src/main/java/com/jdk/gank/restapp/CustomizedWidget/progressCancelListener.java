de64baea71f232c1b934552849ea54219cb9ba7b
江丁魁 <546759734@qq.com>
2016-09-20 14:13:56 +0800

    删除之前的包

--- app/src/main/java/com/jdk/gank/restapp/CustomizedWidget/progressCancelListener.java
+++
@@ -1,8 +0,0 @@
-package com.jdk.gank.restapp.CustomizedWidget;
-
-/**
- * Created by JDK on 2016/8/16.
- */
-public interface progressCancelListener {
-    void onCancelProgress();
-}
--- app/src/main/java/com/jdk/gank/restapp/ModelData/BaseData.java
+++
@@ -1,8 +0,0 @@
-package com.jdk.gank.restapp.ModelData;
-
-/**
- * Created by JDK on 2016/8/2.
- */
-public class BaseData {
-    public boolean error;
-}
